6d9b1111c36d7f899e860f9988e588a3ad49941f
Chance Downs <cdowns@weddingwire.com>
2014-09-09 10:10:33 -0400

    v7.0.2 Re-organize due to source re-organization Merge pips into main file to always include it Use javascript source grunt task to generate base .js and .css instead of having components split out

--- vendor/assets/javascripts/jquery.nouislider.js
+++ vendor/assets/javascripts/jquery.nouislider.js
@@ -1,6 +1,3 @@
 //= require wnumb/wNumb
 //= require libLink/jquery.liblink
-//= require nouislider/module.range
-//= require nouislider/module.options
-//= require nouislider/module.base
-//= require nouislider/optional.pips
+//= require nouislider/jquery.nouislider
